dfc15d76ea4e066fe1d333f702037144fee897cf
Алексей Валерьевич Белоусов <chiisai@yandex.ru>
2021-03-17 23:01:28 +0300

    Вывод слова по буквам

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace SF4._3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Введите Слово!");
+            string word = Console.ReadLine();
+
+            int LenghtOfWord = word.Length;
+
+            Console.WriteLine("Состав Слова:");
+
+            foreach (char s in word)
+            {
+                Console.Write("{0} ", s);
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("В Слове обнаружено {0} букв!", LenghtOfWord);
+
         }
     }
 }
